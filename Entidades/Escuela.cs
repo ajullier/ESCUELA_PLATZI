883b8dff2f47ab5dfeb07e84a8e3e2e721a29ae2
Alejandro Jullier <Alejandro_Jullier@hotmail.com>
2020-11-03 21:12:08 -0300

    Se agregó la interfaz ILugar y se la utilizó en Escuela y Curso. ObjetoEscuelaBase volvió a ser abstracta.

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,21 +1,23 @@
 using System;
 using System.Collections.Generic;
+using CoreEscuela.Util;
 
 namespace CoreEscuela.Entidades
 {
-    public class Escuela: ObjetoEscuelaBase
+    public class Escuela : ObjetoEscuelaBase, ILugar
     {
         public int AñoDeCreación { get; set; }
 
         public string Pais { get; set; }
         public string Ciudad { get; set; }
+        public string Dirección { get; set; }
         public TiposEscuela TipoEscuela { get; set; }
         public List<Curso> Cursos { get; set; }
 
         public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
 
-        public Escuela(string nombre, int año, 
-                       TiposEscuela tipo, 
+        public Escuela(string nombre, int año,
+                       TiposEscuela tipo,
                        string pais = "", string ciudad = "") : base()
         {
             (Nombre, AñoDeCreación) = (nombre, año);
@@ -26,6 +28,16 @@ namespace CoreEscuela.Entidades
         public override string ToString()
         {
             return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
+        }
+
+        void ILugar.LimpiarLugar()
+        {
+            Printer.DrawLine();
+            Console.WriteLine("Limpiando escuela...");
+            foreach (var curso in Cursos){
+                curso.LimpiarLugar();
+            }
+            Console.WriteLine($"Escuela {Nombre} está limpia");
         }
     }
 }
--- Entidades/ObjetoEscuelaBase.cs
+++ Entidades/ObjetoEscuelaBase.cs
@@ -3,7 +3,7 @@ using System;
 namespace CoreEscuela.Entidades
 {
     //Creo una clase abstracta. Como toda idea, no se puede crear una instancia(objeto) de una idea(de la clase ObjetoEscuelaBase).
-    public  class ObjetoEscuelaBase
+    public abstract class ObjetoEscuelaBase
     {
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
